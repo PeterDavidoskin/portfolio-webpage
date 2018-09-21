95926d1d3cc54401007c0ee0f35da86f61d085e7
Peter Pavluchenko <peter.pavluchenko@gmail.com>
2018-09-21 16:38:43 +0200

    add express

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require("express");
+
+var app = express();
+
+app.get("/", function(req, res){
+	res.render("index.html");
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+	console.log("its running!");
+});
